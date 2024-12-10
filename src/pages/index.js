4a2eb6155dd85f41174f52cdab403253f60ba69d
aayashahmad <bhatashu666@gmail.com>
2024-12-10 20:26:05 +0530

    add slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navi from "../components/header"
+import images from "../components/slider/images/images";
+import styles from "../components/slider/slider.module.css"
+import CustomSlider from "../components/slider/customSlider";
 
 
 const App = () => {
   return (
     <>
       <Navi />
+      <CustomSlider>
+        {images.map((image, index) => {
+          return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
+        })}
+      </CustomSlider>
     
     </>
   );
