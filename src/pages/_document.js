9d61c0425c28b73ad1be255b90563059a6cdf97c
“Raqib <raqib850@gmail.com>
2024-12-17 15:52:03 +0530

    fonts added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,7 @@ export default function Document() {
 
       <Head >
           {/* <Navi /> */}  
+          {/* <link src="/css/style.css" ref={"stylesheet"} /> */}
         </Head>
       <body>
      
